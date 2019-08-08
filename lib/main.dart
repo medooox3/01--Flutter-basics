5d4ca7f81e7843112de467418412c96c28954c9d
medooox3 <medooox33@gmail.com>
2019-08-08 09:35:13 +0200

    first commit - learning flutter basics `A journey of a thousand miles begins with a single step`

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:i01_flutter_basics/answer.dart';
+import './question.dart';
+
+main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState(){
+    return _MyAppState();
+  }
+}
+class _MyAppState extends State<MyApp>{ 
+  var _questionIndex = 0;
+  var questions = [
+    {
+      "question text" : "What's your favorite color?",
+      "answers" : ["Red", "Black", "Blue", "White"]
+    },
+    {
+      "question text" : "What's your favorite animal?",
+      "answers" : ["Cat", "Dog", "Horse", "Mouse"]
+    },
+    {
+      "question text" : "What would you like to learn?",
+      "answers" : ["Dart", "Flutter", "Design patterns", "Algorithms"]
+    },
+  ];
+  void _answerQuestion() {
+    setState(() {
+    _questionIndex = _questionIndex >= questions.length -1 ? 0 : _questionIndex + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My First App",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("This is my flutter App"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Question(questions[_questionIndex]["question text"]),
+            ...(questions[_questionIndex]["answers"] as List<String>)
+                  .map((answer){
+                    return Answer(_answerQuestion, answer);
+                  }).toList(),
+          ],
+        ),
+      ),
+    );
+  }
+}
